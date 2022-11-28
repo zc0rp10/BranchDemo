b609b1d1244092ab0c52105bce2fcc2ba59d4d8d
Björn Fällbom <zcorpio@gmail.com>
2022-11-28 16:39:24 +0100

    Added ReactDeveloper Class

--- BranchDemo/Developer.cs
+++ BranchDemo/Developer.cs
@@ -8,6 +8,12 @@ namespace BranchDemo
 {
     internal class Developer
     {
+        public Developer(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
         public string Name { get; set; }
         public int Age { get; set; }
     }
